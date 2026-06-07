d79037e02f03eb171110385c46e79e0d19f7b116
sinjdeveloper <sandamamison1@gmail.com>
2026-06-07 20:40:20 +0300

    init

---
+++ src/main/java/ITU/GreenField/GreenFieldApplication.java
@@ -0,0 +1,13 @@
+package ITU.GreenField;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenFieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenFieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ITU/GreenField/GreenFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package ITU.GreenField;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenFieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
