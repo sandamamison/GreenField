661ee708f0ea0ef002fadb4ea1f0becf53bd6ff0
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-18 08:59:27 +0300

    [update] modify MvtStock table structure by dropping idptdevente and adding refptdevente

--- src/main/resources/sql/18-06-26-Grace01_changeInit.sql
+++ src/main/resources/sql/18-06-26-Grace01_changeInit.sql
@@ -4,3 +4,9 @@ ALTER DATABASE greenfield SET timezone TO 'Indian/Antananarivo';
 
 ALTER TABLE PointDeVente
 ADD COLUMN reference VARCHAR(50) NOT NULL UNIQUE;
+
+ALTER TABLE MvtStock 
+DROP COLUMN idptdevente;
+
+ALTER TABLE MvtStock 
+ADD COLUMN refptdevente VARCHAR(50) REFERENCES PointDeVente(reference);
